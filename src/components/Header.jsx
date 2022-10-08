039d1d734a8d14b74eb72a0c334b648510a6edf7
dove <ndoverte11@gmail.com>
2022-10-08 16:44:14 +0800

    setting up pages and installing react-router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Badge, Container, Dropdown, FormControl, Nav, Navbar } from 'react-bootstrap'
 import { FaShoppingCart } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <a href="/">Shopping Cart</a>
+          <Link href="/">Shopping Cart</Link>
         </Navbar.Brand>
         <Navbar.Text className="search">
           <FormControl style={{ width: 500 }} placeholder="Search a product" className="m-auto" />
