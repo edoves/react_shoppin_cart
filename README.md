edcf14a57b1a5df1021259dfd04b05a600c8dd1f
dove <ndoverte11@gmail.com>
2022-10-08 15:57:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Shopping Cart
+
+Learning reactjs by following this turorial [React Shopping Cart Tutorial | Context API with useReducer Hook in React JS](https://www.youtube.com/watch?v=HptuMAUaNGk&t).
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  )
+}
+
+export default App
