94e00435b32ca2531062935019b07311c1f16bea
dove <ndoverte11@gmail.com>
2022-10-08 22:07:42 +0800

    done coding single product

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,13 +3,14 @@ import { createContext, useContext, useReducer } from 'react'
 import { cartReducer } from './Reducers'
 
 const Cart = createContext()
+faker.seed(99)
 
 const Context = ({ children }) => {
   const products = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.random.image(),
+    img: faker.random.image(),
     inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
     fastDelivery: faker.datatype.boolean(),
     ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
@@ -17,7 +18,7 @@ const Context = ({ children }) => {
 
   const [state, dispatch] = useReducer(cartReducer, { products, cart: [] })
 
-  // console.log(state)
+  console.log(state)
   return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>
 }
 
